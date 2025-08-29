71e199ca6301be6a7644ba983f8010b7120fa3ba
anabarreraj <anabarrera_jauregui@berkeley.edu>
2025-08-29 03:49:27 -0500

    Hello_World and Pytest

---
+++ M00Tutorial01.py
@@ -0,0 +1,16 @@
+"""
+Week 0 Tut: 
+Assignments due -> submit github repo with 
+
+BCourses Notes: 
+Add a python function called 'hello_world' that returns a string
+-Control + shift + p to activate python (anaconda3) interpreter
+"""
+#ANSWER
+
+#Hello World
+def hello_world():
+    
+    return "Hello, World!!"
+
+#pytest (made a test file "test_M00Tutorial01.py")
---
+++ test_M00.py
@@ -0,0 +1,5 @@
+from M00Tutorial01 import hello_world
+
+def test_hello_world():
+    
+    assert hello_world() == "Hello, World!!"
